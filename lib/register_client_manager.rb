6c6c3a07a7e206c819688e47e0b923c63c47c8bb
michaelabenyohai <michaela.benyohai@digital.cabinet-office.gov.uk>
2018-01-29 12:48:34 +0000

    Increase version

--- lib/register_client_manager.rb
+++ lib/register_client_manager.rb
@@ -2,7 +2,7 @@ require 'register_client'
 require 'in_memory_data_store'
 
 module RegistersClient
-    VERSION = '0.8.1'
+    VERSION = '0.9.0'
     class RegisterClientManager
       def initialize(config_options = {})
         @config_options = defaults.merge(config_options)
